b3b53588b1eca890ba2eaede83680dd4d4795401
Usuario <Usuario@CPD02-M09>
2024-03-04 12:04:45 -0300

    adicionado o crud completo

--- ListaDeCompra/App.xaml.cs
+++ ListaDeCompra/App.xaml.cs
@@ -1,7 +1,29 @@
-﻿namespace ListaDeCompra
+﻿using ListaDeCompra.Helpers;
+
+namespace ListaDeCompra
 {
     public partial class App : Application
     {
+        static SQLiteDatabaseHelper _db;
+
+        public static SQLiteDatabaseHelper Db
+        {
+            get
+            {
+                if (_db == null)
+                {
+                    string path = path.Combine(
+                        Environment.GetFolderPath(
+                            Environment.SpecialFolder.LocalApplicationData
+                           ), "banco_sqlite_compras.db3"
+                          );
+
+                    _db = new SQLiteDatabaseHelper( path );
+
+
+                }// Fecha if verificando se _db é null
+            }
+        }
         public App()
         {
             InitializeComponent();
--- ListaDeCompra/Helpers/SQLiteDatabaseHelper.cs
+++ ListaDeCompra/Helpers/SQLiteDatabaseHelper.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using ListaDeCompra.Models;
+﻿using ListaDeCompra.Models;
 using SQLite;
 
 namespace ListaDeCompra.Helpers
@@ -22,6 +17,35 @@ namespace ListaDeCompra.Helpers
         public Task <int> Insert (Produto p)
         {
             return _conn.InsertAsync(p);
+        }
+
+
+        public Task<List<Produto>> Update(Produto p)
+        {
+            string sql = "UPDATE produto SET descricao=?, " +
+                  "Quantidade=?, Preco=? WHERE Id=?";
+
+            return _conn.QueryAsync<Produto>(sql, p.Descricao, p.Quantidade, p.Preco, p.Id);
+        }
+
+
+        public Task<List<Produto>> GetAll()
+        {
+            return _conn.Table.Table<Produto>().ToListAsync();
+        }
+
+        public Task<int> Delete(int id)
+        {
+            return _conn.Table<Produto>().DeleteAsync(i => i.Id == id);
+        }
+
+
+        public Task<List<Produto>> Search (string q)
+        {
+            string sql = "SELECT * FROM Produtos WHERE " +
+                "descricao LIKE '%" + q + "%'";
+
+            return _conn.QueryAsync<Produto>( sql);
         }
     }
 }
