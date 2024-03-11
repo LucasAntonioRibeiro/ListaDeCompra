d2764f6e638fcdd61d75d59d3c99d469bcf0dd05
Usuario <Usuario@CPD03-M12>
2024-03-11 12:18:02 -0300

    adicionado a parte de formulario

---
+++ ListaDeCompra/Views/NovoProduto.xaml.cs
@@ -0,0 +1,40 @@
+﻿using ListaDeCompra.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ListaDeCompra.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class NovoProduto : ContentPage
+    {
+        public NovoProduto()
+        {
+            InitializeComponent();
+        }
+
+        private async void ToolbarItem_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                Produto p = new Produto
+                {
+                    Descricao = txt_descricao.Text,
+                    Quantidade = Convert.ToDouble(txt_quantidade.Text),
+                    Preco = Convert.ToString(txt_preco.text),
+                };
+
+            } catch (Exception ex)
+            {
+                await DisplayAlert("Ops", ex.Message, "OK");
+            }
+        }
+
+       
+    }
+}
